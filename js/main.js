4c0657664deed0ac694061f0a8c74a00b23dd681
Wilfried Musanzi <musanziwilfried@gmail.com>
2022-07-18 12:41:39 +0200

    Login and Signup form: animation

--- js/main.js
+++ js/main.js
@@ -1,1 +1,21 @@
 import "../styles/main.scss";
+
+const btnsClose = document.querySelectorAll(".close");
+const links = document.querySelector(".nav__list");
+let form;
+links.addEventListener("click", (e) => {
+  e.preventDefault();
+  const target = e.target;
+  if (target.classList.contains("nav__link")) {
+    const href = target.getAttribute("href");
+    form = document.querySelector(href);
+    form.classList.add("form__visible");
+  }
+});
+
+btnsClose.forEach((btn) => {
+  btn.addEventListener("click", (e) => {
+    e.preventDefault();
+    form.classList.remove("form__visible");
+  });
+});
