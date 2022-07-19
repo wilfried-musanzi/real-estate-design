3fad8e02cf1979095fff206fc314357ee4c74f90
Wilfried Musanzi <musanziwilfried@gmail.com>
2022-07-19 14:20:28 +0200

    Mobile:3

--- js/main.js
+++ js/main.js
@@ -4,23 +4,23 @@ const btnsClose = document.querySelectorAll(".close");
 const links = document.querySelector(".nav__list");
 const hide = document.querySelectorAll(".hide");
 
-let form;
-links.addEventListener("click", (e) => {
-  e.preventDefault();
-  const target = e.target;
-  if (target.classList.contains("nav__link")) {
-    const href = target.getAttribute("href");
-    form = document.querySelector(href);
-    form.classList.add("form__visible");
-  }
-});
+// let form;
+// links.addEventListener("click", (e) => {
+//   e.preventDefault();
+//   const target = e.target;
+//   if (target.classList.contains("nav__link")) {
+//     const href = target.getAttribute("href");
+//     form = document.querySelector(href);
+//     form.classList.add("form__visible");
+//   }
+// });
 
-btnsClose.forEach((btn) => {
-  btn.addEventListener("click", (e) => {
-    e.preventDefault();
-    form.classList.remove("form__visible");
-  });
-});
+// btnsClose.forEach((btn) => {
+//   btn.addEventListener("click", (e) => {
+//     e.preventDefault();
+//     form.classList.remove("form__visible");
+//   });
+// });
 
 window.addEventListener("load", () => {
   hide.forEach((el) => {
